95040bf131cc364379c73eee373f61a110a1d48f
Ashraf Nazar <ashraf.caspian@gmail.com>
2022-02-20 14:42:05 +0000

    chore: add Chakra UI (#4)
    * chore: add Chakra UI
    
    * Remove yarn.lock file
    
    * Add ESLint dev dependencies
    
    * Update .eslintrc
    
    * Organise directories and files under src directory
    
    Co-authored-by: Ashraf Nazar <ashraf.nazar@and.digital>

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import '../styles/globals.scss';
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
+);
+
+export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => {
   return (
@@ -19,7 +18,7 @@ const Home: NextPage = () => {
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          <code className={styles.code}>pages/home.tsx</code>
         </p>
 
         <div className={styles.grid}>
@@ -59,14 +58,12 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Powered by
+          <span className={styles.logo}></span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
