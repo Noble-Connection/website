9479f8b440c21a46228f2c4c737340524a79d14f
Ashraf Nazar <ashraf.nazar@and.digital>
2022-02-20 23:01:42 +0000

    Use Context API to pass content to components

---
+++ src/contexts/content/types.ts
@@ -0,0 +1,17 @@
+export interface IContentContext {
+  charityName: string;
+  links: {
+    homePage: string;
+    aboutUs: string;
+    contactUs: string;
+    donate: string;
+    facebook: string;
+    twitter: string;
+    youtube: string;
+  };
+  footer: {
+    copyright: string;
+    contactNumber: string;
+    charityRegistrationDetails: string;
+  }
+}
---
+++ src/contexts/content/useContent.ts
@@ -0,0 +1,5 @@
+import { useContext } from 'react';
+import { ContentContext } from './ContentContext';
+import { IContentContext } from './types';
+
+export const useContent = (): IContentContext => useContext<IContentContext>(ContentContext);
