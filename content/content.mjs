e3327967fcfd271917b480bea9de0ab908b3e498
Ashraf Nazar <ashraf.nazar@and.digital>
2022-02-20 23:00:29 +0000

    Add content to pass down DOM

---
+++ content/content.mjs
@@ -0,0 +1,17 @@
+export const content = {
+  charityName: 'Noble Connection',
+  links: {
+    homePage: '/',
+    aboutUs: '/about-us',
+    contactUs: '/contact-us',
+    donate: '/donate',
+    facebook: 'https://www.facebook.com/noble.connection/',
+    twitter: 'https://twitter.com/nobleconnection?lang=en',
+    youtube: 'https://www.youtube.com/channel/UCzVF8TF4o8wWSX3uNgSheFw/featured',
+  },
+  footer: {
+    copyright: 'Copyright ©',
+    contactNumber: 'Tel: 020 3633 8996',
+    charityRegistrationDetails: 'Charity Reg. No. 1186037',
+  },
+};
