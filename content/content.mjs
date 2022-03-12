9e7ebb59d359bfefb159a605f757136e62f33fe4
Ashraf Nazar <ashraf.nazar@and.digital>
2022-03-12 15:40:54 +0000

    Add properties to content file

--- content/content.mjs
+++ content/content.mjs
@@ -1,5 +1,12 @@
 export const content = {
   charityName: 'Noble Connection',
+  contactNumber: '020 3633 8996',
+  emailAddress: 'info@nobleconnection.co.uk',
+  pages: {
+    aboutUs: 'About Us',
+    contactUs: 'Contact Us',
+    donate: 'Donate',
+  },
   links: {
     homePage: '/',
     aboutUs: '/about-us',
@@ -7,11 +14,18 @@ export const content = {
     donate: '/donate',
     facebook: 'https://www.facebook.com/noble.connection/',
     twitter: 'https://twitter.com/nobleconnection?lang=en',
-    youtube: 'https://www.youtube.com/channel/UCzVF8TF4o8wWSX3uNgSheFw/featured',
+    youtube:
+      'https://www.youtube.com/channel/UCzVF8TF4o8wWSX3uNgSheFw/featured',
   },
   footer: {
     copyright: 'Copyright ©',
-    contactNumber: 'Tel: 020 3633 8996',
     charityRegistrationDetails: 'Charity Reg. No. 1186037',
+    address: {
+      registeredOfficeText: 'Registered Office',
+      firstLine: '102 Islip Manor Road',
+      town: 'Northolt',
+      postcode: 'UB5 5EB',
+      country: 'United Kingdom',
+    },
   },
 };
