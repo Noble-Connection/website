ccd64d5dc197450e2d806b626df75b68e3c0c2c8
Ashraf Nazar <ashraf.nazar@and.digital>
2022-04-16 16:59:49 +0100

    Link logo to home page

--- src/components/atoms/Logo/Logo.tsx
+++ src/components/atoms/Logo/Logo.tsx
@@ -1,6 +1,24 @@
+import NextLink from 'next/link';
 import Image from 'next/image';
+import { Link } from '@chakra-ui/react';
+import { useContent } from 'src/contexts/content/useContent';
 import logo from '../../../../public/images/noble-connection-logo.png';
 
-export const Logo = (): JSX.Element => (
-  <Image src={logo} alt="Noble Connection Logo" height={125} width={125} />
-);
+export const Logo = (): JSX.Element => {
+  const {
+    links: { home },
+  } = useContent();
+
+  return (
+    <NextLink href={home} passHref>
+      <Link>
+        <Image
+          src={logo}
+          alt="Noble Connection Logo"
+          height={125}
+          width={125}
+        />
+      </Link>
+    </NextLink>
+  );
+};
