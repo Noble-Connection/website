27ae220079323590facff5ede8cc7ff3ac55c31c
Ashraf Nazar <ashraf.nazar@and.digital>
2022-04-16 17:01:24 +0100

    Update NavbarLinks molecules component

--- src/components/molecules/NavbarLinks/NavbarLinks.tsx
+++ src/components/molecules/NavbarLinks/NavbarLinks.tsx
@@ -1,19 +1,13 @@
-import { useMediaQuery } from '@chakra-ui/react';
 import { NavbarLink } from 'src/components/atoms/NavbarLink/NavbarLink';
 import { useContent } from 'src/contexts/content/useContent';
 
 export const NavbarLinks = (): JSX.Element => {
   const { navigation } = useContent();
-  const [isTablet] = useMediaQuery('(min-width: 769px)');
 
   return (
     <>
       {navigation.map(({ href, label }) => (
-        <NavbarLink
-          href={href}
-          key={label}
-          color={isTablet ? 'blue.900' : 'gray.200'}
-        >
+        <NavbarLink href={href} key={label}>
           {label}
         </NavbarLink>
       ))}
