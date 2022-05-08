153bfdc75347a0444e23cd87530a42bfee83477c
Ashraf Nazar <ashraf.nazar@and.digital>
2022-05-08 22:51:54 +0100

    Use Chakra props over stylesheet for navbar

--- src/components/atoms/NavbarLink/NavbarLink.tsx
+++ src/components/atoms/NavbarLink/NavbarLink.tsx
@@ -1,7 +1,6 @@
 import NextLink from 'next/link';
 import { Link, Text, useMediaQuery } from '@chakra-ui/react';
 import { INavbarLinkProps } from './types';
-import styles from './NavbarLink.module.scss';
 
 export const NavbarLink = ({
   href,
@@ -12,11 +11,12 @@ export const NavbarLink = ({
   return (
     <NextLink href={href} passHref>
       <Link
-        className={styles.navbarLink}
         height={10}
         paddingLeft={isMobile ? '10' : '0'}
         display="flex"
         alignItems="center"
+        color={isMobile ? 'gray.200' : 'blue.900'}
+        _hover={{ textDecoration: 'none' }}
       >
         <Text fontSize="lg">{children}</Text>
       </Link>
